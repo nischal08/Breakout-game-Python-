c2f0e25a8befeb65eba898c4fa3d5bd60db3f07f
nischal08 <nischal.karanjit@gmail.com>
2024-03-13 21:43:41 +0545

    feat; completed ball movement and bounce back when collision with wall and striker partially

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 from turtle import Turtle, Screen
+from ball import Ball
+from striker import Striker
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.title("breakout_game_project6")
+screen.title("Breakout Game")
+screen.tracer(0)
+
+ball = Ball()
+striker = Striker()
+
+screen.listen()
+screen.onkeypress(striker.go_left, "Left")
+screen.onkeypress(striker.go_right, "Right")
+screen.onkeypress(striker.go_left, "a")
+screen.onkeypress(striker.go_right, "d")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall vertically
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with wall horizontally
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.bounce_x()
+
+
+    # Detect collision with striker
+    if ball.distance(striker) < 50:
+        ball.bounce_y()
 
 screen.exitonclick()
+
+
+#TODO:  Detect collision with striker is not stable
---
+++ striker.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+STICKER_Y_POSITION = -230
+STRIKER_SPEED = 20
+
+
+class Striker(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=5, stretch_wid=1)
+        self.penup()
+        self.goto((self.xcor(), STICKER_Y_POSITION))
+
+    def go_left(self):
+        self.penup()
+        self.goto((self.xcor() - STRIKER_SPEED, STICKER_Y_POSITION))
+
+    def go_right(self):
+        self.penup()
+        self.goto((self.xcor() + STRIKER_SPEED, STICKER_Y_POSITION))
