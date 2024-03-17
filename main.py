c84cc745bad444d71af75368710a8340ba98ac44
nischal08 <nischal.karanjit@gmail.com>
2024-03-17 01:08:42 -0700

    feat; made collision logic of ball and brick

--- main.py
+++ main.py
@@ -12,16 +12,30 @@ screen.tracer(0)
 
 ball = Ball()
 striker = Striker()
-
+all_brick = []
 for i in range(1, 13):
     for j in range(1, 7):
-        Brick(coordinate=(-420 + i * 70, 35 + j * 40))
+        all_brick.append(Brick(coordinate=(-420 + i * 70, 35 + j * 40)))
 
 screen.listen()
 screen.onkeypress(striker.go_left, "Left")
 screen.onkeypress(striker.go_right, "Right")
 screen.onkeypress(striker.go_left, "a")
 screen.onkeypress(striker.go_right, "d")
+
+
+def collision():
+    for brick in all_brick:
+        ball_x = ball.xcor()
+        ball_y = ball.ycor()
+        brick_x = brick.xcor()
+        brick_y = brick.ycor()
+        distance = abs(ball_x - brick_x) + abs(ball_y - brick_y)
+        if distance < 40:
+            brick.hideturtle()
+            screen.update()
+            ball.bounce_y()
+
 
 game_is_on = True
 
@@ -45,8 +59,9 @@ while game_is_on:
     # Detect right ball left
     if ball.ycor() < -280:
         ball.reset_position()
+        # game_is_on=False
         # scoreboard.r_point()
-
+    collision()
 screen.exitonclick()
 
 # TODO:  Detect collision with striker is not stable
